d34186067372da5286a72f90e872279da1ce0f3b
Sam Hotiky <178449962@qq.com>
2018-12-07 11:49:49 +0800

    the sortchain component is speed up.

--- sortchain/sortchain.c
+++ sortchain/sortchain.c
@@ -11,11 +11,15 @@
 #include <string.h>
 #include "sortchain.h"
 
-// the sequence is in [0, MAX_SEQ)
-#define MAX_SEQ     (4000000000)     /* the larger the better */
+// the sequence is in [0, MAX_SEQ) nomally, if @THRESHOLD > @MAX_SEQ, the
+// sequence is in [0, THRESHOLD), in other words, the routine can adapt
+// the @MAX_SEQ to @THRESHOLD automatically, so you don't need to change it.
+#define MAX_SEQ     (4294967296 - 1)
+
+#define THRESHOLD   (handle->thres)
 
 /**
- * judge is a @handle full
+ * is the @handle full
  * @param handle: the sort chain handle
  * @return SCH_TRUE : full
  *         SCH_FALSE : not full
@@ -35,12 +39,20 @@ static schres_t find_emptynode(schh_t *handle, schnode_t **node)
 {
     unsigned int i;
 
-    for(i = 0; i < SCH_NODES_TOTAL; i++)
+    if(handle->sparenode != NULL)
     {
-        if(handle->nodes[i].hasdata_flag == SCH_FALSE)
+        *node = handle->sparenode;
+        return SCHRES_OK;
+    }
+    else
+    {
+        for(i = 0; i < THRESHOLD; i++)
         {
-            *node = &handle->nodes[i];
-            return SCHRES_OK;
+            if(handle->nodes[i].hasdata_flag == SCH_FALSE)
+            {
+                *node = &handle->nodes[i];
+                return SCHRES_OK;
+            }
         }
     }
 
@@ -48,54 +60,243 @@ static schres_t find_emptynode(schh_t *handle, schnode_t **node)
 }
 
 /**
- * delete the oldest node of @handle
+ * delete the oldest node from @handle
  * @param handle: the sort chain handle
+ * @return: see @schres_t
  */
-static void delete_oldestdata(schh_t *handle)
+static schres_t delete_oldestdata(schh_t *handle)
 {
-    schnode_t *anynode, *deletenode;
+    schnode_t *delnode;     // delete node
 
-    // if the first one is the oldest one
-    if(handle->head->seq == handle->oldestseq)
+    // get the oldest node
+    fifo_od(&handle->odqh, &delnode);
+
+    // get the deleted node position, so we can get
+    // the mid-node position in @insert_newestdata
+    if(delnode == handle->midnode)
     {
-        anynode = handle->head;
-        handle->head = handle->head->next;
-        anynode->next = NULL;
-        anynode->hasdata_flag = SCH_FALSE;
-        anynode->data = 0;
-        handle->oldestseq++;
-        anynode->seq = 0;
+        handle->del_flag = DELP_MID;
+        handle->ldelnode = handle->midnode->prev;
+    }
+    else
+    {
+        if(delnode->data < handle->midnode->data)
+        {
+            handle->del_flag = DELP_LEFT;
+        }
+        else if(delnode->data > handle->midnode->data)
+        {
+            handle->del_flag = DELP_RIGHT;
+        }
+        else
+        {
+            // if delnode->data == handle->midnode->data
+            if(delnode->sseq < handle->midnode->sseq)
+            {
+                handle->del_flag = DELP_LEFT;
+            }
+            else if(delnode->sseq > handle->midnode->sseq)
+            {
+                handle->del_flag = DELP_RIGHT;
+            }
+            else
+            {
+                return SCHRES_ERR;
+            }
+        }
+    }
+
+    // delete the oldest node
+    if(delnode->next == NULL)
+    {
+        // if @delnode is in the end
+        delnode->prev->next = NULL;
+    }
+    else if(delnode == handle->head)
+    {
+        // if @delnode is in the head
+        handle->head = delnode->next;
+        delnode->next->prev = handle->head;
+    }
+    else
+    {
+        delnode->prev->next = delnode->next;
+        delnode->next->prev = delnode->prev;
+    }
+    memset(delnode, 0, sizeof(schnode_t));
+
+    // update @oldestseq
+    handle->oldestseq++;
+
+    // update @sparenode
+    handle->sparenode = delnode;
+
+    return SCHRES_OK;
+}
+
+///////////////////////////////////////
+static void print_all(schh_t *handle, schdat_t data)
+{
+#if 0
+    schnode_t *anynode;
+    static int aa;
+
+    if(handle->full_flag == 1 && aa == 0)
+    {
+        aa = 1;
+        printf("-------------------------------------\n");
+    }
+
+    for(anynode = handle->head; anynode != NULL; anynode = anynode->next)
+    {
+        printf("%d(%d) ", anynode->data, anynode->sseq);
+    }
+
+    if(handle->full_flag == 1)
+    {
+        printf("  ---  data=%d, mid=%d\n", data, handle->midnode->data);
+    }
+    else
+    {
+        printf("  ---  data=%d, mid=NA\n", data);
+    }
+
+#endif
+}
+///////////////////////////////////////
+
+/**
+ * update the sseq of @handle, the sseq should not exceed the @MAX_SEQ
+ * the sseq: same-sequence
+ * what is it for: in a sort-chain, there may are many same datas, they
+ *                 lay in line. that's not a problem, but when we delete
+ *                 the oldest data from a sort-chain while the oldest
+ *                 node's data == mid node's data, how can we know the
+ *                 oldest node is in the right of the mid node or left?
+ *                 so we need sseq, when you insert a data to sort-chain
+ *                 if it's data equals one of the datas in the sort-chain,
+ *                 it's sseq will add itself 1. however, the sseq should
+ *                 not exceed the @MAX_SEQ
+ * @param handle: the sort chain handle
+ * @param node: the node that will update it's sseq
+ */
+static void update_sseq(schh_t *handle, schnode_t *node)
+{
+    unsigned int sseq_min;
+    schnode_t *anynode;
+
+    if(node == handle->head || node->data != node->prev->data)
+    {
         return;
     }
 
-    // if the oldest one isn't in the first place
-    for(anynode = handle->head; anynode != NULL; anynode = anynode->next)
+    (*node).sseq = node->prev->sseq + 1;
+    if((*node).sseq >= MAX_SEQ)
     {
-        if((anynode->next->seq == handle->oldestseq))
+        // if the @sseq reachs to the top, reset it
+        sseq_min = (*node).sseq - (handle->thres - 1);
+        for(anynode = node; anynode->data == anynode->prev->data;
+            anynode = anynode->prev)
         {
-            deletenode = anynode->next;
-            anynode->next = deletenode->next;
-            deletenode->next = NULL;
-            deletenode->hasdata_flag = SCH_FALSE;
-            handle->oldestseq++;
-            deletenode->seq = 0;
-            deletenode->data = 0;
-            return;
+            if(anynode == handle->head)
+            {
+                break;
+            }
+            anynode->sseq -= sseq_min;
+        }
+        anynode->sseq -= sseq_min;
+    }
+}
+
+/**
+ * insert a data bigger then @lin to @handle
+ * @param handle: the sort chain handle
+ * @param node: the node stores the inserted data
+ */
+static void insert_biggerdata(schh_t *handle, schnode_t *node)
+{
+    schnode_t *anynode;
+
+    for(anynode = handle->lin; anynode != NULL; anynode = anynode->next)
+    {
+        if(anynode->next != NULL)
+        {
+            if(node->data < anynode->next->data)
+            {
+                // if anynode <= data < anynode->next, then the data would
+                // lay between them
+                (*node).next = anynode->next;
+                anynode->next->prev = node;
+                anynode->next = node;
+                (*node).prev = anynode;
+                break;
+            }
+            else
+            {
+                // nothing to do here
+            }
+        }
+        else
+        {
+            // if anynode <= data < anynode.next==NULL, then the data
+            // would be in the end position
+            (*node).next = NULL;
+            (*node).prev = anynode;
+            anynode->next = node;
+            break;
         }
     }
 }
 
 /**
- * insert a data to @handle
+ * insert a data smaller then @lin to @handle
  * @param handle: the sort chain handle
- * @param data: the data to be inserted
- * @note: this function assumes that the @handle is not full. so be sure
- *        the @handle is not full before calling this function.
+ * @param node: the node stores the inserted data
+ */
+static void insert_smallerdata(schh_t *handle, schnode_t *node)
+{
+    schnode_t *anynode;
+
+    if(node->data < handle->head->data)
+    {
+        // data < head, we place it in the left of the head, and make
+        // it be the head
+        (*node).next = handle->head;
+        handle->head->prev = node;
+        handle->head = node;
+        (*node).prev = handle->head;
+        return;
+    }
+
+    for(anynode = handle->lin; /* nothing here */; anynode = anynode->prev)
+    {
+        if(node->data >= anynode->prev->data)
+        {
+            // if anynode->prev <= data < anynode, then the data would lay
+            // between them
+            (*node).next = anynode;
+            (*node).prev = anynode->prev;
+            (*node).prev->next = node;
+            (*node).next->prev = node;
+            break;
+        }
+        else
+        {
+            // nothing to do
+        }
+    }
+}
+
+/**
+ * insert the newest data to @handle
+ * @param handle: the sort chain handle
+ * @param data: the inserted data
+ * @return: see @schres_t
  */
 static schres_t insert_newestdata(schh_t *handle, schdat_t data)
 {
-    schnode_t *node;
-    schnode_t *anynode;
+    schnode_t *node, *anynode;
+    unsigned int i;
 
     // find an empty node to fill
     if(find_emptynode(handle, &node) != SCHRES_OK)
@@ -108,55 +309,93 @@ static schres_t insert_newestdata(schh_t *handle, schdat_t data)
     (*node).seq = handle->newestseq++;
     (*node).hasdata_flag = SCH_TRUE;
     (*node).next = NULL;
-    if(handle->newestseq - handle->oldestseq == 1)
+    (*node).prev = NULL;
+    if(handle->head == NULL)
     {
         // if there is only one data in sort chain now
-        return SCHRES_OK;
+        handle->head = node;
+        (*node).prev = handle->head;
+        goto after_inserting;
     }
 
-    // insert the data
-    for(anynode = handle->head; anynode != NULL; anynode = anynode->next)
+    // inserting the @data
+    if(data >= handle->lin->data)
     {
-        // if data > head, make it compare to the latter datas one by one until
-        // we get it's position
-        if(anynode->data < data)
+        insert_biggerdata(handle, node);
+    }
+    else
+    {
+        insert_smallerdata(handle, node);
+    }
+    update_sseq(handle, node);
+
+after_inserting:
+
+    // updating related stuff
+    if(handle->full_flag == SCH_FALSE
+       && (handle->newestseq - handle->oldestseq) >= handle->thres)
+    {
+        handle->full_flag = SCH_TRUE;
+    }
+    handle->lin = node;
+
+    // push the current node address to fifo
+    fifo_in(&handle->odqh, &node);
+
+    // get mid-value
+    if(handle->del_flag != DELP_NONE)
+    {
+        switch(handle->del_flag)
         {
-            // if anynode < data < anynode.next, then the data could lay
-            // between them
-            if(anynode->next != NULL)
+        case DELP_LEFT:
+            if(data >= handle->midnode->data)
             {
-                if(data < anynode->next->data)
-                {
-                    (*node).next = anynode->next;
-                    anynode->next = node;
-                    break;
-                }
+                handle->midnode = handle->midnode->next;
             }
             else
             {
-                // if anynode < data < anynode.next==NULL, then the data
-                // would be in the end position
-                anynode->next = node;
-                (*node).next = NULL;
-                break;
+                // nothing to do here
             }
-        }
-        else
-        {
-            // data <= head, we place it in the left of the head, and make
-            // it be the head
-            (*node).next = handle->head;
-            handle->head = node;
             break;
+
+        case DELP_RIGHT:
+            if(data >= handle->midnode->data)
+            {
+                // nothing to do here
+            }
+            else
+            {
+                handle->midnode = handle->midnode->prev;
+            }
+            break;
+
+        case DELP_MID:
+            if(data >= handle->ldelnode->data)
+            {
+                handle->midnode = handle->ldelnode->next;
+            }
+            else
+            {
+                handle->midnode = handle->ldelnode;
+            }
+            break;
+
+        default:
+            return SCHRES_ERR;
         }
     }
-
-    // set the chain's full flag
-    if(handle->full_flag != SCH_TRUE)
+    else
     {
-        if(handle->newestseq - handle->oldestseq >= SCH_NODES_TOTAL)
+        if(handle->full_flag == SCH_TRUE)   // if this is the first time we come here
         {
-            handle->full_flag = SCH_TRUE;
+            for(anynode = handle->head, i = 0; anynode != NULL; anynode = anynode->next, i++)
+            {
+                if(i == THRESHOLD / 2)
+                {
+                    handle->midnode = anynode;
+                    break;
+                }
+            }
         }
     }
 
@@ -186,7 +425,7 @@ static void reset_all_seq(schh_t *handle)
 {
     unsigned int i;
 
-    for(i = 0; i < SCH_NODES_TOTAL; i++)
+    for(i = 0; i < THRESHOLD; i++)
     {
         handle->nodes[i].seq -= handle->oldestseq;
     }
@@ -201,30 +440,27 @@ static void reset_all_seq(schh_t *handle)
  */
 static void get_mid(schh_t *handle, schdat_t *mid)
 {
-    unsigned int i;
-    schnode_t *anynode = handle->head;
-
-    for(i = 0; i < SCH_NODES_TOTAL / 2; i++)
-    {
-        anynode = anynode->next;
-    }
-    *mid = anynode->data;
+    *mid = handle->midnode->data;
 }
 
 /**
  * sort chain initialization
  * @param handle: the sort chain handle
+ * @param thres: threshhold. the sortchain will pop out oldest data when
+ *              the number of data reaches @thres
+ *                @sortchain_add act fastest
  * @return: see @schres_t
  */
-schres_t sortchain_init(schh_t *handle)
+schres_t sortchain_init(schh_t *handle, unsigned int thres)
 {
-    if(!handle)
+    if(!handle || !thres || thres > SCH_NODES_TOTAL)
     {
         return SCHRES_ERR;
     }
 
     memset(handle, 0, sizeof(schh_t));
-    handle->head = &handle->nodes[0];
+    handle->thres = thres;
+    fifo_init(&handle->odqh, handle->odqb, sizeof(schnode_t *), handle->thres + 1);
     return SCHRES_OK;
 }
 
@@ -245,7 +481,10 @@ schres_t sortchain_add(schh_t *handle, schdat_t data, schdat_t *mid)
 
     if(is_full(handle) == SCH_TRUE)
     {
-        delete_oldestdata(handle);
+        if(delete_oldestdata(handle) != SCHRES_OK)
+        {
+            return SCHRES_ERR;
+        }
         if(insert_newestdata(handle, data) != SCHRES_OK)
         {
             return SCHRES_ERR;
@@ -255,6 +494,8 @@ schres_t sortchain_add(schh_t *handle, schdat_t data, schdat_t *mid)
             reset_all_seq(handle);
         }
         get_mid(handle, mid);
+
+        print_all(handle, data);
     }
     else
     {
@@ -262,22 +503,39 @@ schres_t sortchain_add(schh_t *handle, schdat_t data, schdat_t *mid)
         {
             return SCHRES_ERR;
         }
+
+        print_all(handle, data);
+
+        if(is_full(handle) == SCH_TRUE)
+        {
+            if(is_seqtop(handle) == SCH_TRUE)
+            {
+                reset_all_seq(handle);
+            }
+            get_mid(handle, mid);
+            return SCHRES_OK;
+        }
+
         return SCHRES_NOTREADY;
     }
 
     return SCHRES_OK;
 }
 
-int demo_main(void)
+// right output-------9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 7 7
+
+int sortchain_demo_main(void)
 {
     schres_t res;
     schh_t datalib;
     schdat_t mid;
-    schdat_t dat[] = {5, 9, 1, 8, 6, 2, 0, 3, 4, 7};
-    /// schdat_t dat[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     unsigned int i;
+    schdat_t dat[] = {
+        5, 10, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 17, 17, -13, 1, 9,
+        19, 4, -7, 16, 9, 9, 9, 9, 13, 9, -14, 16, 7, 7, 3, -5, 11,
+    };
 
-    if(sortchain_init(&datalib) != SCHRES_OK)
+    if(sortchain_init(&datalib, 8) != SCHRES_OK)
     {
         printf("init error!\n");
         return -1;
--- sortchain/sortchain.h
+++ sortchain/sortchain.h
@@ -10,37 +10,56 @@
 #ifndef _SORTCHAIN_H_
 #define _SORTCHAIN_H_
 
+#include "fifo.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // user configure here
 typedef int schdat_t;       /* sort chain data */
-#define SCH_NODES_TOTAL     (5)
-
-#if ((SCH_NODES_TOTAL / 2 * 2) == SCH_NODES_TOTAL)
-#   error the @SCH_NODES_TOTAL must be odd!
-#endif
-
-#if (SCH_NODES_TOTAL == 1)
-#   error the @SCH_NODES_TOTAL can not be 1!
-#endif
+#define SCH_NODES_TOTAL       (100)
 
 #define SCH_TRUE    (1)
 #define SCH_FALSE   (0)
 
+typedef enum {
+    DELP_NONE = 0,
+    DELP_LEFT,
+    DELP_RIGHT,
+    DELP_MID,
+} delp_t;                                  // delete position
+
 typedef struct SCHNODE {
     schdat_t data;
-    unsigned int seq;
+    unsigned int seq;                      // sequence, the data inserted history
+    unsigned int sseq;                     // same-sequence, if a data equals to the previous
+                                           // it's sseq += 1
     char hasdata_flag;
-    struct SCHNODE *next;
+    struct SCHNODE *next;                  // next node
+    struct SCHNODE *prev;                  // previous node
 } schnode_t;   // sort chain node
 
 typedef struct {
-    schnode_t nodes[SCH_NODES_TOTAL];   // TODO  should be configurable here
+    schnode_t nodes[SCH_NODES_TOTAL];      // TODO  should be configurable here
     char full_flag;
-    unsigned int oldestseq;     // the oldest sequence
-    unsigned int newestseq;     // the newest sequence, note that the variable
-                                // represents the new coming data seq, the real
-                                // newest seq in sort chain is @newestseq - 1
+    unsigned int oldestseq;                // the oldest sequence
+    unsigned int newestseq;                // the newest sequence, note that the variable
+                                           // represents the new coming data seq, the real
+                                           // newest seq in sort chain is @newestseq - 1
     schnode_t *head;
-} schh_t;
+    unsigned int thres;                    // threshold, the window size of the this handle
+    struct NODE odqh;                      // oldest data queue handle
+    schnode_t *odqb[SCH_NODES_TOTAL];      // oldest data queue buffer
+    schnode_t *sparenode;                  // when a node had been deleted from a sortchain, it
+                                           // becomes a spare-node
+    schnode_t *lin;                        // last-insert-node
+
+    delp_t del_flag;                       // the deleted node position refering to midnode
+    schnode_t *midnode;
+    schnode_t *ldelnode;                   // left-delete-node. the node in the left of the
+                                           // deleted node
+} schh_t;                                  // sort-chain handle
 
 typedef enum {
     SCHRES_OK,         // ok
@@ -48,7 +67,11 @@ typedef enum {
     SCHRES_ERR,        // error
 } schres_t;            // sort chain result
 
-schres_t sortchain_init(schh_t *handle);
+schres_t sortchain_init(schh_t *handle, unsigned int thres);
 schres_t sortchain_add(schh_t *handle, schdat_t data, schdat_t *mid);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
