dabb545710103cc34f781fe5534d2fe524aad301
sam hotiky <178449962@qq.com>
2019-09-06 22:49:52 +0800

    refine the sortchain demo

--- sortchain/main.c
+++ sortchain/main.c
@@ -1,41 +1,75 @@
 #include <stdio.h>
+#include <math.h>
 #include "sortchain.h"
 
-// right output-------9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 7 7 
+#define PI          (3.1415926)
+#define ARRAY_CNT   (40)
+#define ERR_PRT()   printf("error in line: %d\n", __LINE__)
+
+static void get_sin_array(schdat_t *p, int len, int period)
+{
+    int i;
+    int k = 2 * PI / period;
+
+    for(i = 0; i < len; i++)
+    {
+        p[i] = 10 * sin(k * i); 
+    }
+}
+
 int main(void)
 {
-    schres_t res;
     schh_t datalib;
+    schres_t res;
+    schdat_t buf [ARRAY_CNT];
+    schdat_t buf1[ARRAY_CNT];
+    schdat_t buf2[ARRAY_CNT];
     schdat_t mid;
-    unsigned int i;
-    schdat_t dat[] = {
-        5, 10, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 17, 17, -13, 1, 9,
-        19, 4, -7, 16, 9, 9, 9, 9, 13, 9, -14, 16, 7, 7, 3, -5, 11,
-    };
+    int i;
+    int ofs = 98;
+    int period = 8;
+    int dummy;
 
-    if(sortchain_init(&datalib, 8) != SCHRES_OK)
+    printf("start...\n");
+    // prepare the resource array: buf1, buf2
+    get_sin_array(buf1, ARRAY_CNT, period);
+    for(i = 0; i < ARRAY_CNT; i++)
     {
-        printf("init error!\n");
+        buf2[i] = buf1[i] + ofs;
+    }
+
+    // filt the buf1 to buf
+    if(sortchain_init(&datalib, period) != SCHRES_OK)
+    {
+        ERR_PRT();
         return -1;
     }
-
-    for(i = 0; i < sizeof(dat) / sizeof(dat[0]); i++)
+    for(dummy = 0, i = 0; i < ARRAY_CNT; i++)
     {
-        res = sortchain_add(&datalib, dat[i], &mid);
-        if(res == SCHRES_OK)
+        res = sortchain_add(&datalib, buf1[i], &mid);
+        if(res == SCHRES_NOTREADY)
         {
-            printf("%d ", mid);
+            dummy++;
         }
-        else if(res == SCHRES_NOTREADY)
+        else if(res == SCHRES_ERR)
         {
-            printf("-");
-        }
-        else
-        {
-            printf("run error!\n");
+            ERR_PRT();
             return -2;
         }
+        buf[i] = buf1[i] - mid;
     }
+
+    // now the buf is filted, verify it with buf2
+    for(i = dummy; i < ARRAY_CNT; i++)
+    {
+        if(buf[i] != buf2[2] - ofs)
+        {
+            ERR_PRT();
+            return -3;
+        }
+    }
+
+    printf("ok\n");
     return 0;
 }
 
